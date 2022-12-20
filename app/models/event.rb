84bc1c77066af74ab528f357d254df24960c5b3d
hui00 <kevinbrinkschulte@hotmail.de>
2022-12-20 01:15:55 +0100

    add event model
    add event model

---
+++ app/models/event.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :bigint           not null, primary key
+#  date       :datetime
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Event < ApplicationRecord
+end
