2324226d8de5fd5cf091ec52e0a5051afab4c81e
hui00 <kevinbrinkschulte@hotmail.de>
2022-12-20 03:10:51 +0100

    improve routes with as

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "users/index", to: "users#index", as: "users"
+  get "users/:id", to: "users#show", as: "user"
   get "event/index"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
